5fe5d629dbc6f27263094b56756a5adaf8506717
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-15 14:47:56 +0530

    added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,89 @@
-import Image from "next/image";
+import{
+    BrainCogIcon,
+    EyeIcon,
+    GlobeIcon,
+    MonitorSmartphoneIcon,
+    ServerCogIcon,
+    ZapIcon,
+} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from 'next/image';
+
+const features = [
+    {
+        name: "Store your pdf documents",
+        description: "Keep all your PDF files securely stored and easily accessible anytime, anywhere.",
+        icon: GlobeIcon,
+    },
+    {
+        name: "BLazing Fast Responses",
+        description: "Experience lightning fast answer to your queries, ensuring you get the information you need instantly.",
+        icon: ZapIcon,
+    },
+    {
+        name: "Chat Memorisation",
+        description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+        icon: BrainCogIcon,
+    },
+    {
+        name: "Interactive PDF Viewer",
+        description: "Engage with your PDFs like never before using our ntutive and interactive viewer.",
+        icon: EyeIcon,
+    },
+    {
+        name: "Cloud Backup",
+        description: "Rest assured knowing your documents are safely backed up on the cloud,protected from loss or damage.",
+        icon: ServerCogIcon,
+    },
+    {
+        name: "Responsive Across Devices",
+        description: "Access and chat with your PDF seamlessly from any device, whether it's your desktop, tablet or smartphone.",
+        icon: MonitorSmartphoneIcon,
+    },
+]
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to accent-indigo-400">
+        <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+            <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl sm:text-center">
+                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Your Interactive Document Companion</h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl ">
+                        Transform Your PDFs into Interactive Conversations
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        Introducing{""}
+                        <span className="font-bold text-indigo-600">Chat with PDF.</span>
+                        <br/>
+                        <br/>
+                        Upload your document and our chatbot will answer questions, summarize content, and answer all your Qs.
+                        Ideal for everyone, <span className="text-indigo-600">Chat with PDF</span>{""}
+                        turns static documents into {""}
+                        <span className="font-bold">dynamic conversations</span>, enhancing productivity 10x fold effortlessly.
+                    </p>
+                </div>
+                <Button asChild className="mt-10">
+                    <Link href="/dashboard">Get Started</Link>
+                </Button>
+            </div>
+            <div className="relative overflow-hidden pt-16">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <Image
+                    src="https://i.imgur.com/VciRSTI.jpeg"
+                    alt = "App Screenshot"
+                    width={2432}
+                    height={1442}
+                    className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+                    />
+                    <div aria-hidden="true" className="relative">
+                        <div></div>
+                    </div>
+                </div>
+            </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+// import type { NextConfig } from "next";
+//
+// const nextConfig: NextConfig = {
   /* config options here */
-};
-
-export default nextConfig;
+    module.exports = {
+        images: {
+            remotePatterns: [new URL('https://i.imgur.com/VciRSTI.jpeg/**')],
+        },
+    }
+// };
+//
+// export default nextConfig;
