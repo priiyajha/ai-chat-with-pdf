b1540eddd887a0ee79f1c169ca53ca59abc7fc56
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-18 12:55:49 +0530

    built dashboard ui

---
+++ components/PlaceholderDocument.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+import {Button} from "@/components/ui/button";
+import {PlusCircleIcon} from "lucide-react";
+import {useRouter} from "next/navigation";
+
+const PlaceholderDocument = () => {
+
+    const router = useRouter();
+
+    function handleClick() {
+        router.push("/dashboard/upload");
+    }
+
+    return (
+        <Button onClick={handleClick} className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200
+        drop-shadow-md text-gray-400 ">
+            <PlusCircleIcon className="h-16 w-16"/>
+            <p> Add a Document </p>
+        </Button>
+    );
+}
+
+
+export default PlaceholderDocument;
