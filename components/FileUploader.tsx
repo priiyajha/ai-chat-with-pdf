4f6ff85e9831d190fe1f5d0da2a322bddae382d1
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-18 18:46:48 +0530

    added upload page

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,6 +1,6 @@
 "use client";
 import {useDropzone} from 'react-dropzone'
-import {useCallback} from "react";
+import {useCallback, useEffect} from "react";
 import {
     CheckCheckIcon,
     CircleArrowDown,
@@ -9,11 +9,39 @@ import {
     SaveIcon,
 }from "lucide-react";
 
+import useUpload from '@/hooks/useUpload';
+
+
+import { useRouter } from 'next/navigation';
+
+
+
 const FileUploader = () => {
-    const onDrop = useCallback((acceptedFiles: File []) => {
+
+    const  {progress, status, fileId, handleUpload } = useUpload();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(fileId){
+            router.push(`/dashboard/files/${fileId}`);
+        }
+    }, [fileId,router]);
+
+    const onDrop = useCallback(async (acceptedFiles: File []) => {
         // Do something with the files
+        const file = acceptedFiles[0];
+        if(file){
+            await handleUpload(file);
+        }else{
+
+        }
     }, [])
-    const {getRootProps, getInputProps, isDragActive, isFocused, isDragAccept} = useDropzone({onDrop})
+    const {getRootProps, getInputProps, isDragActive, isFocused, isDragAccept} =
+        useDropzone({onDrop,
+            maxFiles:1,
+        accept:{
+            "application/pdf": [".pdf"],
+        }})
 
     return (
         <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto ">
---
+++ hooks/useUpload.ts
@@ -0,0 +1,71 @@
+'use client';
+
+import {useState} from "react";
+import {useUser} from "@clerk/nextjs";
+import {useRouter} from "next/navigation";
+import { v4 as uuidv4 } from 'uuid';
+
+import { ref, uploadBytesResumable, getDownloadURL } from "@firebase/storage";
+import { doc, setDoc } from "@firebase/firestore";
+import { storage, db } from "@/firebase";
+
+
+export enum StatusText {
+    UPLOADING = "Uploading file...",
+    UPLOADED = "File uploaded successfully...",
+    SAVING = "Saving file to database...",
+    GENERATING = "Generating AI Embeddings, this will only take a few seconds..."
+}
+
+export type Status = StatusText[keyof StatusText];
+
+function useUpload () {
+    const [progress, setProgress] = useState<number | null>(null);
+    const [fileId, setFileId] = useState<string | null>(null);
+    const [status, setStatus] = useState<Status | null>(null);
+    const {user} = useUser();
+    const router = useRouter();
+    const handleUpload = async (file: File) => {
+        if (!file || !user) return;
+
+        const fileIdToUploadTo = uuidv4();
+
+        const storageRef = ref(storage,
+            `users/${user.id}/files/${fileIdToUploadTo}`
+        );
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setStatus(StatusText.UPLOADING);
+                setProgress(percent);
+            },
+            (err) => {
+                console.log(err);
+            }, async () => {
+                setStatus(StatusText.UPLOADED);
+
+                const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+                setStatus(StatusText.SAVING);
+                await setDoc(doc(db, "users", user.id, 'files', fileIdToUploadTo), {
+                    name: file.name,
+                    size: file.size,
+                    type: file.type,
+                    downloadUrl: downloadUrl,
+                    ref: uploadTask.snapshot.ref.fullPath,
+                    createdAt: new Date(),
+                })
+                setStatus(StatusText.GENERATING);
+                setFileId(fileIdToUploadTo);
+
+            }
+        );
+    };
+    return {progress, status, fileId, handleUpload};
+}
+
+export default useUpload;
