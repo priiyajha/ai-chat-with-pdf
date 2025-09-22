8da08e09aa76ace77d7beca78ca726f491995b01
priiyajha <jhapriiyaa2104@gmail.com>
2025-09-22 16:13:58 +0530

    adding changes

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -70,7 +70,7 @@ function Chat({id}:{id:string}) {
 
         setMessage(newMessages);
 
-    }, [snapshot, message]);
+    }, [snapshot]);
 
 
     const handleSubmit = async function(e:FormEvent) {
